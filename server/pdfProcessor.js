1bc9fc210441feceb789a1d9d181d9a69d5f6dd1
Patricio Brito <patriciobritodesign@gmail.com>
2024-12-23 20:45:01 +0000

    Delete server directory

--- server/pdfProcessor.js
+++
@@ -1,9 +0,0 @@
-// pdfProcessor.js
-import { PDFDocument } from 'pdf-lib';
-
-export async function gerarPDF(tema, numPaginas, imageUrl, author) {
-    const pdfDoc = await PDFDocument.create();
-    const page = pdfDoc.addPage();
-    page.drawText(`Tema: ${tema}\nAutor: ${author}\nNúmero de páginas: ${numPaginas}`);
-    return pdfDoc.save();
-}
