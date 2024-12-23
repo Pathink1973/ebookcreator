42781fd9b3ff91fe319b1ec7e08a0b84a2d36599
Patricio Brito <patriciobritodesign@gmail.com>
2024-12-23 22:00:45 +0000

    Add files via upload
    update

--- netlify/functions/chromium.js
+++ netlify/functions/chromium.js
@@ -1,35 +1,18 @@
-import path from 'path';
-import { getReadableDirectory } from 'lambdafs';
-
-const CHROME_PATH = path.join(
-  'chrome-aws-lambda',
-  'bin',
-  process.platform === 'linux' ? 'chromium' : 'chrome'
-);
+import chromium from '@sparticuz/chromium';
 
 export async function getChromium() {
-  const directory = await getReadableDirectory();
-  const executablePath = path.join(directory, CHROME_PATH);
+  await chromium.init();
   
-  const chromium = {
-    executablePath,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-gpu',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-extensions'
-    ],
+  const options = {
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
     defaultViewport: {
       width: 1920,
       height: 1080,
       deviceScaleFactor: 1,
-    },
-    headless: true
+    }
   };
 
-  return chromium;
+  return options;
 }
